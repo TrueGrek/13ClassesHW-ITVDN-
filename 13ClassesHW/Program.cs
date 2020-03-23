bea43efcdc5e2ad530c210849aa3373f74d277d1
Grek <grrekov@yandex.ru>
2020-03-23 16:57:47 +0300

    Добавьте файлы проекта.

---
+++ 13ClassesHW/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+namespace _13ClassesHW
+{
+    class Program
+    {
+        static int deep;
+        static void Recursion()
+        {
+            Console.WriteLine("{0} say \"Hello\"", Thread.CurrentThread.Name);
+            Thread.Sleep(1);
+            Thread thread2 = new Thread(Recursion);
+            deep++;
+            thread2.Name = "Thread " + deep;
+            thread2.Start();
+        }
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(Recursion) { Name = "Thread " + deep };
+            thread.Start();
+        }
+    }
+}
